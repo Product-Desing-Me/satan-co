ff8fc229140ce5672b40244b2be4346b072b4adb
emiliano <emilianogbarrientos@gmail.com>
2025-09-24 20:38:19 -0300

    cambiando url del link de fotos y organizando el layout

--- frontend/src/components/Cards/ProductCard/ProductCard.tsx
+++ frontend/src/components/Cards/ProductCard/ProductCard.tsx
@@ -13,28 +13,29 @@ export default function CatalogCard({ product }: ProductCardProps) {
         <GoToDetail id={product.id} title={product.title} description={product?.description || ""} seed={product?.seed || []}>
             <article
                 id={product.id}
-                className="flex flex-col text-align-left lg:w-[377px] lg:h-[417px] md:w-[275px] md:h-[320px] w-[156px] h-[164px] md:mx-6 lg:mx-0 text-left mb-4 font-poppins mt-4"
+                className="flex flex-col text-align-left items-center text-left mb-4 font-poppins mt-4"
             >
-                <div className="relative group  lg:rounded-3xl rounded-xl overflow-hidden sm:border-3 border-2 bg-borderpinkgradient bg-clip-border border-transparent group lg:mb-4 mb-1">
+                <div className="relative group rounded-3xl overflow-hidden border-2 bg-borderpinkgradient bg-clip-border border-transparent w-fit h-fit flex items-center justify-center">
+                <div className="flex items-center justify-center bg-[#2A1540] rounded-xl
+                    ">
                     { !product.image ?
-                        <div className="flex items-center justify-center lg:w-[375px] lg:h-[360px] md:w-[275px] md:h-[220px] w-[156px] h-[133px]">
-                            <Spinner size="100" colorText="#FAFAFA" />
-                        </div>
+                        <Spinner size="100" colorText="#FAFAFA" />
                         :
                         <Image
                             src={product.image}
                             alt={product.title}
-                            width={165}
-                            height={133}
-                            className="object-cover transition-opacity duration-300 group-hover:opacity-60 lg:w-[375px] lg:h-[360px] md:w-[275px] md:h-[220px] w-[156px] h-[133px] "
+                            width={375}
+                            height={360}
+                            className="object-cover transition-opacity duration-300 group-hover:opacity-60 rounded-xl"
                         />
                     }
-                    <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity duration-300"></div>
-                    <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-buttongradiant shadow-lg text-white text-xl py-2 px-4 rounded-3xl opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                        Ver detalle
-                    </div>
                 </div>
-                <div className="p-0 mt-1">
+                <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity duration-300"></div>
+                <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-buttongradiant shadow-lg text-white text-xl py-2 px-4 rounded-3xl opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    Ver detalle
+                </div>
+            </div>
+                <div className="p-0 mt-1 w-full">
                     <p className="text-textColor3 lg:text-2xl text-sm truncate font-normal">{product.title}</p>
                     <p className="text-textColor2 lg:text-xl text-sm font-normal truncate">{Array.isArray(product.seed) && product.seed.map((seed: string) => seed.toUpperCase()).join(", ")}</p>
                 </div>
